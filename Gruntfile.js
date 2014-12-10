138f0042468ead10f54a91e34852e8654bfa7300
Haribabu Pa <haribabupa@live.com>
2014-12-10 12:17:06 +0530

    Ignore added
    An ignore file is added for ignoring the unwanted files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,35 @@
 module.exports=function(grunt){
     
     grunt.initConfig({
+        pkg:grunt.file.readJSON("package.json"),
         doPrint:{
             main:{
-                pName:"Hello grunt"
+                pName:"Hello grunt : <%=pkg.name + ' v' + pkg.version %>"
+            }
+        },
+        app:{
+            create:{
+                
+            }
+        },
+        exec:{
+            echo_forme:{
+                command:'echo HELLO HARI'
+            },
+            echo_cpath:{
+                command:'%CD%'
+            },
+            create:{
+                command:'cordova create <%=pkg.name.toLowerCase()%> <%=pkg.base_id + "." + pkg.name%> <%=pkg.name.toLowerCase()%>',
             }
         }
-    })
-    
-    grunt.registerMultiTask('doPrint', 'This is a sample before actual', function (){
-        console.log(this.data.pName);
         
-    });
+    })
+
+    grunt.loadNpmTasks('grunt-exec');
+    
+    grunt.registerTask('sayhello', ['exec:echo_forme']);
+    grunt.registerTask('getpath', ['exec:echo_cpath']);
+    grunt.registerTask('create', ['exec:create']);
+    
 }
